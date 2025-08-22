5312744b9afabddb5ce2e135663974ea59c882ed
vivekkalal09 <vivekkalal09@gmail.com>
2025-08-22 14:09:04 +0530

    script.js button reference added

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+let optionButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingAream = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scirptButtons = document.querySelectorAll(".script");
 
+// List of fontlist
+let fontList = ["Arial", "Verdana", "Times New Roman", "Garamond", "Georgia", "Courier New", "cursive"]
+
+//Initial Settings
+const initializer = () => {
+    //function calls for highlighting buttons
+    // No highlights for link, unlink, lists, undo, redo since they are one time operations
+    highlighter(alignButtons, true);
+    highlighter(spacingButtons, true);
+    highlighter(formatButtons, false);
+    highlighter(scirptButtons, true)
+};
+
+// Hightlight clicked button
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click", () => {
+            //needsRemoval = true means only one button should be highlight and other would be normal 
+        })
+    });
+
+}
